1cdbe3e94511974b6c12ced79e1922acf3e4c932
DavidYu00 <dy2262@nyu.edu>
2025-04-24 20:09:33 -0400

    change sys path of import

--- web-app/tests/test_models.py
+++ web-app/tests/test_models.py
@@ -3,10 +3,10 @@ import bcrypt
 import mongomock
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../auth/')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from bson.objectid import ObjectId
 from unittest.mock import patch
-from models import User
+from auth.models import User
 
 # Mocked MongoDB collection
 mock_db = mongomock.MongoClient().db
